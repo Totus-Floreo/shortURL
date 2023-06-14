dd071f772f78ce64d4b27f6fa910982a77493982
Timur Kulakov <kulakovcore@gmail.com>
2023-06-14 07:58:27 +0300

    Add: New Error for len short > 10

--- internal/app/domain/errors.go
+++ internal/app/domain/errors.go
@@ -3,6 +3,7 @@ package domain
 import "errors"
 
 var (
+	ErrorInvalidShort    = errors.New("invalid short link")
 	ErrorInvalidLink     = errors.New("invalid link")
 	ErrorInvalidDecode   = errors.New("link cant decode")
 	ErrorLinkNotFound    = errors.New("link not found")
